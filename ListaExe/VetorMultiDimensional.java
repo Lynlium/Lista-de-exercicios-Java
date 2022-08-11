77bc5a59e8cefc4b6963e85210eca57a3df457e9
Michelle Barreto <michelle.pinheirobarreto@gmail.com>
2022-08-11 17:07:46 -0300

    Ex SomandoQuadrados e VetorMultiDimensional

--- ListaExe/VetorMultiDimensional.java
+++ ListaExe/VetorMultiDimensional.java
@@ -8,6 +8,35 @@ public class VetorMultiDimensional {
     public static void main(String[] args) {
         ArrayList<String> nomes = new ArrayList<String>();
         ArrayList<String> sobrenomes = new ArrayList<String>();
+        ArrayList<String>nomesEsobrenomes = new ArrayList<>();
+nomes.add("Marie");
+nomes.add("Connie");
+nomes.add("Alfredo");
+nomes.add("Frederico");
+nomes.add("Jinora");
+nomes.add("Tupi");
+nomes.add("Mommi");
+nomes.add("Minji");
+nomes.add("Pizis");
+nomes.add("Bire");
+
+sobrenomes.add("Courrie");
+sobrenomes.add("Head");
+sobrenomes.add("Chatão");
+sobrenomes.add("Fredinho");
+sobrenomes.add("Jiji");
+sobrenomes.add("Tupinha");
+sobrenomes.add("Gata");
+sobrenomes.add("Mini-Ginga");
+sobrenomes.add("Pizocks");
+sobrenomes.add("cadê meu dragão?");
+
+for (int i = 0; i < nomes.size(); i++){
+
+    nomesEsobrenomes.add(nomes.get(i));
+    nomesEsobrenomes.add(sobrenomes.get(i));
+    System.out.println("Nomes e Sobrenomes: "+ nomesEsobrenomes);
+}
 
     }
 }
