7d554e06f01b7be9ca54fa3879884299559cb60e
Michelle Barreto <michelle.pinheirobarreto@gmail.com>
2022-08-11 16:46:37 -0300

    Ex SomandoQuadrados e VetorMultiDimensional

--- ListaExe/VetorMultiDimensional.java
+++ ListaExe/VetorMultiDimensional.java
@@ -1,8 +1,13 @@
 package ListaExe;
+
+import java.util.ArrayList;
+
 //Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos,
 // cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
 public class VetorMultiDimensional {
     public static void main(String[] args) {
+        ArrayList<String> nomes = new ArrayList<String>();
+        ArrayList<String> sobrenomes = new ArrayList<String>();
 
     }
 }
